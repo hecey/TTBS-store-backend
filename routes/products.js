3fe088d2940d15c510d1869b4618056053935dd2
Hecey <hecey@hotmail.com>
2022-10-13 01:02:00 -0500

    Added search by name in products service

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ const products = require('../services/products');
 
 router.get('/', async function(req, res, next) {
   try {
-    res.json(await products.getMultiple(req.query.page,req.query.categoryId));
+    res.json(await products.getMultiple(req.query.page,req.query.categoryId,req.query.name));
   } catch (err) {
     console.error(`Error while getting products `, err.message);
     next(err);
--- services/products.js
+++ services/products.js
@@ -1,25 +1,32 @@
-const db = require('./db');
-const helper = require('../helper');
-const config = require('../config');
+const db = require("./db");
+const helper = require("../helper");
+const config = require("../config");
 
-async function getMultiple(page = 1, categoryId){
+async function getMultiple(page = 1, categoryId, name) {
   const offset = helper.getOffset(page, config.listPerPage);
   const category = helper.searchCategory(categoryId);
+  const search = categoryId
+    ? ` p.category='${category}' `
+    : name
+    ? ` p.name like '%${name}%' `
+    : '';
+  const where = search != "" ? `where ${search}` : "";
+
   const rows = await db.query(
     `SELECT p.id, p.name, p.url_image, p.price, p.discount
     FROM product as p
-    WHERE p.category='${category}'
+    ${where}
     LIMIT ${offset},${config.listPerPage}`
   );
   const data = helper.emptyOrRows(rows);
-  const meta = {page};
+  const meta = { page };
 
   return {
     data,
-    meta
-  }
+    meta,
+  };
 }
 
 module.exports = {
-  getMultiple
-}
+  getMultiple,
+};
